170775586249826061262b23337908f5c0e5b716
Daanvdv <35851026+Daanvdv@users.noreply.github.com>
2020-07-24 16:00:18 +0200

    Improved player movement with CharacterController

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,32 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Options")]
     public float movementSpeed = 8.0f;
+    public float gravitySpeed = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private GameObject cam;
+    private Vector3 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        controller = this.gameObject.AddComponent<CharacterController>();
+        cam = new GameObject("Camera", typeof(Camera));
+        cam.transform.position = new Vector3(cam.transform.position.x, 15.0f, cam.transform.position.z);
+        cam.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movementAmount = movementSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.W))
-        {
-            this.transform.position += new Vector3(0.0f, 0.0f, movementAmount);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.position += new Vector3(-movementAmount, 0.0f, 0.0f);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            this.transform.position += new Vector3(0.0f, 0.0f, -movementAmount);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.position += new Vector3(movementAmount, 0.0f, 0.0f);
-        }
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(movement * Time.deltaTime * movementSpeed);
+
+        cam.transform.position = new Vector3(this.transform.position.x, cam.transform.position.y, this.transform.position.z);
     }
 }
